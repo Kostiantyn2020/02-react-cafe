168175c1e5718f4fe17b81fc81824839750d4073
Kostiantyn2020 <161701157+Kostiantyn2020@users.noreply.github.com>
2025-11-30 09:24:36 +0100

    4

--- vite-project/src/components/VoteOptions/VoteOptions.tsx
+++ vite-project/src/components/VoteOptions/VoteOptions.tsx
@@ -1,42 +1,3 @@
-// import { VoteType } from "../../types/votes";
-// import css from "./VoteOptions.module.css";
-
-// interface VoteOptionsProps {
-//   onVote: (type: VoteType) => void;
-//   onReset: () => void;
-//   canReset: boolean;
-// }
-
-// function VoteOptions({ onVote, onReset, canReset }: VoteOptionsProps) {
-//   return (
-//     <div className={css.container}>
-//       {/* <button className={css.button} onClick={() => onVote("good")}>
-//         Good
-//       </button>
-
-//       <button className={css.button} onClick={() => onVote("neutral")}>
-//         Neutral
-//       </button>
-
-//       <button className={css.button} onClick={() => onVote("bad")}>
-//         Bad
-//       </button>
-
-//       {canReset && (
-//         <button className={`${css.button} ${css.reset}`} onClick={onReset}>
-//           Reset
-//         </button> */}
-
-//       <button className={css.button}>Good</button>
-//       <button className={css.button}>Neutral</button>
-//       <button className={css.button}>Bad</button>
-//       <button className={`${css.button} ${css.reset}`}>Reset</button>
-//     </div>
-//   );
-// }
-
-// export default VoteOptions;
-
 import type { VoteType } from "../../types/votes";
 import css from "./VoteOptions.module.css";
 
@@ -61,7 +22,6 @@ function VoteOptions({ onVote, onReset, canReset }: VoteOptionsProps) {
         Bad
       </button>
 
-      {/* 👉 Показуємо Reset тільки якщо є голоси */}
       {canReset && (
         <button className={`${css.button} ${css.reset}`} onClick={onReset}>
           Reset
--- vite-project/src/components/VoteStats/VoteStats.tsx
+++ vite-project/src/components/VoteStats/VoteStats.tsx
@@ -1,34 +1,10 @@
-// import styles from "./VoteStats.module.css";
-
-// const VoteStats = ({ votes, totalVotes, positiveRate }) => {
-//   return (
-//     <div className={styles.container}>
-//       <p className={styles.stat}>
-//         Good: <strong>0</strong>
-//       </p>
-//       <p className={styles.stat}>
-//         Neutral: <strong>0</strong>
-//       </p>
-//       <p className={styles.stat}>
-//         Bad: <strong>0</strong>
-//       </p>
-//       <p className={styles.stat}>
-//         Total: <strong>0</strong>
-//       </p>
-//       <p className={styles.stat}>
-//         Positive: <strong>0%</strong>
-//       </p>
-//     </div>
-//   );
-// };
-
 import type { Votes } from "../../types/votes";
 import styles from "./VoteStats.module.css";
 
 interface VoteStatsProps {
   votes: Votes;
   totalVotes: number;
-  positiveRate: number; // целое число процента, например 42
+  positiveRate: number;
 }
 
 export default function VoteStats({
@@ -56,5 +32,3 @@ export default function VoteStats({
     </div>
   );
 }
-
-// export default VoteStats;
